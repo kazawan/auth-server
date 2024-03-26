81dc800877be5301ae1acc43280d1112d8b33864
kazawan <55086403@qq.com>
2024-03-26 15:47:13 +0800

    modified:   todoRoutes/index.js 	modified:   tokenBlackList.txt

--- todoRoutes/index.js
+++ todoRoutes/index.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { findUserAllTodo } from '../db/index.js'
+import {verifyToken} from '../jwt/index.js'
+
+
+
 const TodoRoutes = Router()
 
 
 TodoRoutes.post('/all', async (req, res) => {
-    const { email } = req.body;
+    const { accessToken } = req.body;
+    const {email} = verifyToken(accessToken)
+    if(!email){
+        res.send({
+            code:403,
+            message:"token is invalid"
+        })
+        return
+    }
     const data = await findUserAllTodo(email);
     res.send({
         code:200,
